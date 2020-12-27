0c1f0e1e1d30c833a46f495601fde5d449c356dd
Sagar Alwani <46413586+SagarAlwani@users.noreply.github.com>
2020-12-27 20:18:43 +0530

    Add files via upload

---
+++ Averageof_two/average.c
@@ -0,0 +1,13 @@
+	#include<conio.h>               //header file
+	#include<stdio.h>               //header file
+	int main()
+	{int a,b;
+	float avg;       //avg=average of two numbers
+	printf("enter the two numbers");
+	scanf("%d %d",&a,&b);
+	avg = (a+b)/2;                    //logic
+	printf("the average of two numbers is %.2f",avg);
+	getch();
+	}
+
+
