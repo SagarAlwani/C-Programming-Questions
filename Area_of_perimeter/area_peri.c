c816e61f67935738568a38b0250fd999f7dd78dc
Sagar Alwani <46413586+SagarAlwani@users.noreply.github.com>
2020-12-27 18:08:58 +0530

    Add files via upload

---
+++ Area_of_perimeter/area_peri.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+	void main()
+	{
+		float pi,r;
+		pi=3.14,r=4.2;
+		printf("value of pi : 3.14\n");
+		printf("value of radius : 4.2\n");
+		printf("Area of Circle = %f\n",pi*r*r);
+		printf("Perimeter of Circle =%f\n",2*pi*r);
+		getch();
+	}
