206c580e0041c1a33f7e01b41d3bdbbc2f0c79dc
Sagar Alwani <46413586+SagarAlwani@users.noreply.github.com>
2020-12-27 20:21:09 +0530

    Add files via upload

---
+++ Armstrong/armstrong.c
@@ -0,0 +1,20 @@
+#include<conio.h>
+#include<stdio.h>
+	void main()
+	{
+		int arm=0,n,i,temp;
+		printf("Enter a Number to be Checked:\n");
+		scanf("%d",&n);
+		temp=n;
+		for(;n!=0;)
+		{
+			i=n%10;
+			arm=arm+(i*i*i);
+			n=n/10;
+		}
+		if(arm==temp)
+		printf("%d is an Armstrong Number",temp);
+		else
+		printf("%d is not an Armstrong Number",temp);
+		getch();
+	}
