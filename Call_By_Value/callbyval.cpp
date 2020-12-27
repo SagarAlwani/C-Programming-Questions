867340246397df343dcd76e1532713899ab12932
Sagar Alwani <46413586+SagarAlwani@users.noreply.github.com>
2020-12-27 20:14:23 +0530

    Add files via upload

---
+++ Call_By_Value/callbyval.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void swap(int,int);
+int main()
+{
+        int b,a;
+        cout<<"Enter First Number(A)\n";
+        cin>>a;
+        cout<<"Enter Second Number(B)\n";
+        cin>>b;
+        cout<<"Before Swapping the Value : \n"<<"A = "<<a<<"\n"<<"B = "<<b<<"\n";
+        swap(a,b);
+        cout<<"\n (Outside Function) After Swapping Value : \n"<<"A = "<<a<<"\n"<<"B = "<<b<<"\n";
+}
+void swap(int a,int b)
+{
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        cout<<"\n (Inside Function) After Swapping Value : \n"<<"A = "<<a<<"\n"<<"B = "<<b;
+}
