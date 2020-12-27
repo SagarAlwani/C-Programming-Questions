1b340333fb646572385ab42df17f710c11b3f973
Sagar Alwani <46413586+SagarAlwani@users.noreply.github.com>
2020-12-27 18:09:16 +0530

    Add files via upload

---
+++ Area_of_Rectangle/area_of_rectangle.c
@@ -0,0 +1,14 @@
+//Area of a rectangle
+#include<stdio.h>
+#include<conio.h>
+	void main()
+	{
+		int l,b,a;
+		printf("Enter Length of the Rectangle\n");
+		scanf("%d",&l);
+		printf("Enter Breadth of the Rectangle\n");
+		scanf("%d",&b);
+		a=l*b;
+		printf("The Area of the Rectangle is:%d\n",an);
+		getch();	
+	}
