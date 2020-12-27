595d6c588f72ced8fd8876af6dc204cec57eecdf
Sagar Alwani <46413586+SagarAlwani@users.noreply.github.com>
2020-12-27 18:09:33 +0530

    Add files via upload

---
+++ Arithmetic_Operation/Arithmetic.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+	void main()
+	{
+		int a,b;
+		a=20,b=10;
+		printf("Value of 1st Number = 20\n");
+		printf("Value of 2nd Number = 10\n");
+		printf("The Addition of two Numbers is : %d\n",a+b);
+		printf("The Subtraction of two Numbers is : %d\n",a-b);
+		printf("The Multiplication of two Numbers is : %d\n",a*b);
+		printf("The Division of two Numbers is : %d\n",a/b);
+		printf("The Remainder of two Numbers is : %d",a%b);
+		getch();
+	}
